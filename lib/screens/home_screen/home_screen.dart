cdd3d70c8988d0d35fa77e411f3be9e20371bb7e
leolon <guslonnrle@student.gu.se>
2020-12-14 15:02:23 +0100

    homescreen fixad, poppen funkar inte

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,8 +1,71 @@
 import 'package:flutter/material.dart';
 
+import '../main_screen.dart';
+
 class HomeScreen extends StatelessWidget {
+
+final Widget _favorites =Icon(Icons.star, color: Colors.white, size: 70);
+final Widget _search = Icon(Icons.search, color: Colors.white, size: 70);
+final Widget _notifications = Icon(Icons.notifications, color: Colors.white, size: 70);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Welcome to Crypto 296', 
+          style: TextStyle(
+            fontSize: 30
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            _customButton(context, 'Favorites', _favorites),
+            _customButton(context, 'Search', _search),
+            _customButton(context, 'Notifications', _notifications),
+          ],),
+      )
+    );
+  }
+
+
+  Widget _customButton(context, String title, Widget icon) {
+    return Padding(padding: EdgeInsets.only(top: 60),
+      child: Container(
+        padding: EdgeInsets.only(top: 30, bottom: 30),
+        alignment: FractionalOffset.center,
+        color: Theme.of(context).primaryColor,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 30,right:30.0),
+              child: 
+                icon,
+            ),
+          
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MainScreen(),
+                  ),
+                );
+              },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child:  Text(
+                title,
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white)),
+            ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
