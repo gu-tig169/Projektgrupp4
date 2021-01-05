30105b121bca53c2ccb4d2d6657e25eb8e5a4708
luh <luh@pop-os.localdomain>
2021-01-05 11:27:05 +0100

    Null check for Shrimpy api

--- lib/global_widgets/percentage_text.dart
+++ lib/global_widgets/percentage_text.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 Widget percentageText(double percentage, TextStyle style) {
-  TextStyle textStyle = TextStyle(
-      color: (percentage < 0) ? Colors.red : Colors.green,
-      fontWeight: style.fontWeight,
-      fontSize: style.fontSize);
+  Color _color;
+  if (percentage.toString() == 'NaN') {
+    _color = Colors.grey;
+  } else if (percentage < 0) {
+    _color = Colors.red;
+  } else {
+    _color = Colors.green;
+  }
 
-  return Text('${percentage.toStringAsFixed(2)}%', style: textStyle);
+  // (percentage.toString() == 'NaN') ? (percentage < 0) ? Colors.red : Colors.green
+  TextStyle textStyle =
+      TextStyle(color: _color, fontWeight: style.fontWeight, fontSize: style.fontSize);
+
+  return Text((percentage.toString() == 'NaN') ? 'No data' : '${percentage.toStringAsFixed(2)}%',
+      style: textStyle);
 }
--- lib/models/currency.dart
+++ lib/models/currency.dart
@@ -31,9 +31,9 @@ class Currency {
     return Currency(
         name: json['name'],
         symbol: json['symbol'],
-        priceUsd: double.parse(json['priceUsd']),
-        priceBtc: double.parse(json['priceBtc']),
-        percentChange24hUsd: double.parse(json['percentChange24hUsd']),
+        priceUsd: double.parse(json['priceUsd'] ?? 'NaN'),
+        priceBtc: double.parse(json['priceBtc'] ?? 'NaN'),
+        percentChange24hUsd: double.parse(json['percentChange24hUsd'] ?? 'NaN'),
         lastUpdated: json['lastUpdated']);
   }
 
