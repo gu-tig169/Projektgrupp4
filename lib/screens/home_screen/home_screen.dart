9b270728eaed60a38886b1d9f84cf0099272d5fa
AxelFilipsson <filipsson960409@gmail.com>
2021-01-06 16:23:57 +0100

    Firebase fungerar nu för IOS + lite homescreen pill ;)

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -5,24 +5,28 @@ import '../main_screen.dart';
 class HomeScreen extends StatelessWidget {
   final Widget _favorites = Icon(Icons.star, color: Colors.white, size: 40);
   final Widget _search = Icon(Icons.search, color: Colors.white, size: 40);
-  final Widget _notifications = Icon(Icons.notifications, color: Colors.white, size: 40);
+  final Widget _notifications =
+      Icon(Icons.notifications, color: Colors.white, size: 40);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Crypto 296',
-        ),
-      ),
-      body: Center(
-        child: ListView(
-          children: [
-            _customButton(context, 'Favorites', _favorites, 2),
-            _customButton(context, 'Search', _search, 1),
-            _customButton(context, 'Notifications', _notifications, 0),
-          ],
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   title: Text(
+      //     'Crypto 296',
+      //   ),
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 70.0),
+        child: Center(
+          child: ListView(
+            children: [
+              _customButton(context, 'Favorites', _favorites, 2),
+              _customButton(context, 'Search', _search, 1),
+              _customButton(context, 'Notifications', _notifications, 0),
+            ],
+          ),
         ),
       ),
     );
@@ -77,13 +81,15 @@ class HomeScreen extends StatelessWidget {
         );
       },
       child: Padding(
-          padding: EdgeInsets.only(top: 40),
+          padding: EdgeInsets.only(top: 90),
           child: Container(
             margin: EdgeInsets.only(left: 24.0, right: 24.0),
             width: 340,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [BoxShadow(blurRadius: 5.0, offset: Offset.fromDirection(3.0))],
+                boxShadow: [
+                  BoxShadow(blurRadius: 5.0, offset: Offset.fromDirection(3.0))
+                ],
                 gradient: LinearGradient(
                   colors: [
                     Theme.of(context).primaryColor,
@@ -100,7 +106,8 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Container(
                   padding: const EdgeInsets.all(8),
-                  child: Text(title, style: TextStyle(fontSize: 25, color: Colors.white)),
+                  child: Text(title,
+                      style: TextStyle(fontSize: 25, color: Colors.white)),
                 ),
               ],
             ),
